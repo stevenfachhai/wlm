0e9e1bfc0c63f3051fc144120fe2a110c81ec264
stevenfachhai <stevenbfachhai2@gmail.com>
2022-03-04 17:21:38 +0530

    Home page

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,59 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-     color: Colors.yellow, 
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Text(
+                'Welcome to',
+                style: TextStyle(fontSize: 40),
+              ),
+              Text(
+                'Worship lyrics Mara/Mizo',
+                 style: TextStyle(fontSize: 40),
+              ),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Mara',
+                    style: TextStyle(fontSize: 30),
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Mizo',
+                    style: TextStyle(fontSize: 30),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
