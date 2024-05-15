4209744a834bec99b91c4c53d682acbeec3dc2b4
Stevenfachhai <your_email@example.com>
2024-05-15 19:53:05 +0530

    change favorite screen

--- lib/screens/favorite_screen/favorite_screen.dart
+++ lib/screens/favorite_screen/favorite_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:wlm/screens/favorite_screen/favorite_screen_manager.dart';
 import 'package:wlm/screens/lyrics_screen/lyrics_screen.dart';
 import 'package:wlm/song/song.dart';
@@ -26,30 +27,73 @@ class _FavoriteScreenState extends State<FavoriteScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Favorite Songs'),
+        backgroundColor: Color.fromARGB(255, 232, 232, 226),
+        title: Text(
+          'Worship Lyrics Mara&Mizo',
+          style: GoogleFonts.libreBaskerville(
+            textStyle: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.normal,
+            ),
+          ),
+        ),
       ),
-      body: ValueListenableBuilder<List<String>>(
-          valueListenable: manager.favoritesNotifier,
-          builder: (context, favoriteSongs, child) {
-            return ListView.builder(
-              itemCount: favoriteSongs.length,
-              itemBuilder: (context, index) {
-                final id = favoriteSongs[index];
-                final song = getSongFromId(id);
-                return ListTile(
-                  title: Text(song.title),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => LyricsScreen(song: song),
-                      ),
-                    );
-                  },
-                );
-              },
-            );
-          }),
+      body: Column(
+        children: [
+          TextField(
+            onChanged: (searchString) {
+              manager.search(searchString);
+            },
+            autofocus: true,
+            decoration: InputDecoration(
+              hintText: 'search',
+              suffixIcon: const Icon(Icons.search),
+              filled: true,
+              fillColor: Colors.grey.shade200,
+            ),
+          ),
+          Expanded(
+            child: ValueListenableBuilder<List<String>>(
+                valueListenable: manager.favoritesNotifier,
+                builder: (context, favoriteSongs, child) {
+                  return ListView.builder(
+                    itemCount: favoriteSongs.length,
+                    itemBuilder: (context, index) {
+                      final id = favoriteSongs[index];
+                      final song = getSongFromId(id);
+                      return ListTile(
+                        title: Text(
+                          '${index + 1}. ${song.title}',
+                          style: const TextStyle(fontSize: 17),
+                        ),
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => LyricsScreen(song: song),
+                            ),
+                          );
+                        },
+                      );
+                    },
+                  );
+                }),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.popUntil(context, (route) => route.isFirst);
+        },
+        child: const Icon(Icons.home),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomAppBar(
+        shape: const CircularNotchedRectangle(),
+        child: Container(
+          height: 50.0,
+        ),
+      ),
     );
   }
 }
--- lib/screens/favorite_screen/favorite_screen_manager.dart
+++ lib/screens/favorite_screen/favorite_screen_manager.dart
@@ -6,10 +6,33 @@ import 'package:wlm/song/song_info.dart';
 
 class FavoriteScreenManager {
   final favoritesNotifier = ValueNotifier<List<String>>([]);
+  List<String> favorites = [];
+
+  void search(String searchString) {
+    favoritesNotifier.value = favorites
+        .where(
+          (title) => _contains(title, searchString),
+        )
+        .toList();
+  }
+
+  bool _contains(String title, String searchString) {
+    return title.toLowerCase().contains(searchString.toLowerCase());
+  }
 
   Future<void> loadFavorites() async {
     final settings = getIt<UserSettings>();
     final favorites = await settings.getFavorites();
     favoritesNotifier.value = favorites;
+  }
+
+  Song getSong(String title) {
+    final songDetails = songInfo.firstWhere((song) => song['title'] == title);
+    return Song(
+      id: songDetails['id']!,
+      title: songDetails['title']!,
+      lyrics: songDetails['lyrics']!,
+      link: songDetails['link']!,
+    );
   }
 }
